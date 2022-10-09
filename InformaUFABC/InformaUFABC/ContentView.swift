1f146c47d75dd8e17b4098b9e0d21edc29e2d783
Sara Felix <sfelix@lifescan.com>
2022-10-09 08:47:20 -0300

    using NavigationView

--- InformaUFABC/InformaUFABC/ContentView.swift
+++ InformaUFABC/InformaUFABC/ContentView.swift
@@ -6,61 +6,14 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 20) {
-
-            Text("Informa UFABC").font(.largeTitle).bold()
-                .foregroundColor(Color("Grinch"))
-
-                .padding(.all)
-            HStack(spacing: 20) {
-                Button(action: {}) {
-                    HStack{
-                        Text("Ru")
-                            .fontWeight(.bold)
-                    }
-                    .padding()
-                    .background(Color .yellow)
-                    .foregroundColor(Color("Grinch"))
-                    .clipShape(Capsule())
-                }
-
-                Button(action: {}) {
-                    HStack{
-                        Text("Fretado")
-                            .fontWeight(.bold)
-                    }
-                }
-                .padding()
-                .background(Color .yellow)
-                .foregroundColor(Color("Grinch"))
-                .clipShape(Capsule())
-
-                Button(action: {}) {
-                    HStack{
-                        Text("Agenda")
-                            .fontWeight(.bold)
-                    }
-                }
-                .padding()
-                .background(Color .yellow)
-                .foregroundColor(Color("Grinch"))
-                .clipShape(Capsule())
+        NavigationView {
+            VStack {
             }
-        }
-    }
-
-    struct GrowingButton: ButtonStyle {
-        func makeBody(configuration: Configuration) -> some View {
-            configuration.label
-                .padding()
-                .background(.blue)
-                .foregroundColor(.white)
-                .clipShape(Capsule())
-                .scaleEffect(configuration.isPressed ? 1.2 : 1)
-                .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+            .navigationTitle("INFORMA UFABC")
         }
     }
 }
@@ -68,6 +21,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-.previewInterfaceOrientation(.landscapeRight)
     }
 }
